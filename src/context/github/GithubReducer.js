8f9f730f1f4cbe3a11cd7198526ad017f03aa353
magdrey <dreyjay24@gmail.com>
2022-09-12 02:24:35 +0100

    initial c

--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -1,45 +1,41 @@
 const githubReducer = (state, action) => {
-    switch (action.type) {
-        case 'GET_USERS':
-            return {
-                ...state, 
-                users: action.payload,
-                loading: false
-            }
+  switch (action.type) {
+    case "GET_USERS":
+      return {
+        ...state,
+        users: action.payload,
+        loading: false,
+      };
 
-        case 'GET_USER':
-            return{
-                ...state,
-                user: action.payload,
-                loading: false,
-            }
+    case "GET_USER":
+      return {
+        ...state,
+        user: action.payload,
+        loading: false,
+      };
 
-        case 'GET_REPOS':
-            return{
-                ...state,
-                repos: action.payload,
-                loading: false,
-            }
-        
-        case 'SET_LOADING':
-            return({
-                ...state,
-                loading: true
-            })
-            
-        case 'CLEAR_USERS':
-            return({
-                ...state,
-                users: action.payload,
-                
-            })
-    
-        default: 
-        return (state)
+    case "GET_REPOS":
+      return {
+        ...state,
+        repos: action.payload,
+        loading: false,
+      };
 
-            
-    }
-}
+    case "SET_LOADING":
+      return {
+        ...state,
+        loading: true,
+      };
 
+    case "CLEAR_USERS":
+      return {
+        ...state,
+        users: [],
+      };
 
-export default githubReducer
+    default:
+      return state;
+  }
+};
+
+export default githubReducer;
